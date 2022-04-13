aaf7394c3bfa4715f30c0c835ad19d94aa9b6a2b
Birk <birk.kensen@gmail.com>
2022-04-13 11:31:06 +0200

    added env file

--- index.js
+++ index.js
@@ -6,6 +6,11 @@ const PORT = 8080;
 
 const app = express();
 
+const uri =
+  'mongodb+srv://ij:bIkK3zoLag3hcX3R@<cluster-url>?retryWrites=true&writeConcern=majority';
+
+const client = new MongoClient(uri);
+
 app.use(express.static('public/images'));
 
 app.listen(PORT, () => {
