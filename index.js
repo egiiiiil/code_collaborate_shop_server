a9fbf5091a49fbaab1a1240d640a0e00bd8641c9
Birk <birk.kensen@gmail.com>
2022-04-13 13:16:46 +0200

    added env file example and changed user / password to it in the uri

--- index.js
+++ index.js
@@ -1,15 +1,30 @@
 import express from 'express';
 import mongodb from 'mongodb';
+import 'dotenv/config';
 import cors from 'cors';
 
 const PORT = 8080;
 
 const app = express();
 
-const uri =
-  'mongodb+srv://ij:bIkK3zoLag3hcX3R@<cluster-url>?retryWrites=true&writeConcern=majority';
+const uri = `mongodb+srv://${process.env.USER_NAME}:${process.env.PASSWORD}@code-collaborate-shop.4vuoy.mongodb.net/test?retryWrites=true&writeConcern=majority`;
+const client = new mongodb.MongoClient(uri);
 
-const client = new MongoClient(uri);
+async function run() {
+  try {
+    await client.connect();
+
+    const database = client.db('cnc-shop-db');
+    const productsCollection = database.collection('products');
+
+    const query = { type: 'wheels' };
+    const skateboards = await productsCollection.find(query).toArray();
+    console.log(skateboards);
+  } finally {
+    await client.close();
+  }
+}
+run().catch(console.dir);
 
 app.use(express.static('public/images'));
 
