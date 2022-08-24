de974a721196969287ae0939daa87324937bba51
egiiiiil <79319021+egiiiiil@users.noreply.github.com>
2022-08-24 11:39:04 +0200

    cors

--- index.js
+++ index.js
@@ -12,7 +12,7 @@ const app = express();
 app.use(express.json());
 app.use(
 	cors({
-		origin: 'http://localhost:3000',
+		origin: 'https://skatebordslol.netlify.com',
 	})
 );
 
--- src/controllers/productsController.test.js
+++ src/controllers/productsController.test.js
@@ -7,86 +7,86 @@ import request from 'request';
 // request (in this case) is like a fetch
 
 describe('productsController', function () {
-  it('Status Code 200 from Server', function (done) {
-    request(
-      'http://localhost:8080/api/products/',
-      function (error, response, body) {
-        expect(response.statusCode).to.equal(200);
-        done();
-      }
-    );
-  });
+	it('Status Code 200 from Server', function (done) {
+		request(
+			'https://skatebordslol.herokuapp.com/api/products/',
+			function (error, response, body) {
+				expect(response.statusCode).to.equal(200);
+				done();
+			}
+		);
+	});
 
-  it('Find specific itemId', function (done) {
-    request(
-      'http://localhost:8080/api/products/62553684729c44d7ab0bd7a8',
-      function (error, response, body) {
-        expect(response.statusCode).to.equal(200);
-        done();
-      }
-    );
-  });
+	it('Find specific itemId', function (done) {
+		request(
+			'https://skatebordslol.herokuapp.com/api/products/62553684729c44d7ab0bd7a8',
+			function (error, response, body) {
+				expect(response.statusCode).to.equal(200);
+				done();
+			}
+		);
+	});
 
-  it('Get Jart Deck Renaissance III', function (done) {
-    request(
-      'http://localhost:8080/api/products/62553684729c44d7ab0bd7a8',
-      function (error, response, body) {
-        const { brand, name, type } = JSON.parse(body);
-        expect(response.statusCode).to.equal(200);
-        expect(brand).to.equal('Jart');
-        expect(type).to.equal('deck');
-        expect(name).to.equal('Renaissance III');
-        done();
-      }
-    );
-  });
+	it('Get Jart Deck Renaissance III', function (done) {
+		request(
+			'https://skatebordslol.herokuapp.com/api/products/62553684729c44d7ab0bd7a8',
+			function (error, response, body) {
+				const { brand, name, type } = JSON.parse(body);
+				expect(response.statusCode).to.equal(200);
+				expect(brand).to.equal('Jart');
+				expect(type).to.equal('deck');
+				expect(name).to.equal('Renaissance III');
+				done();
+			}
+		);
+	});
 });
 
 describe('Get products', function () {
-  it('Check total numbers of items', function (done) {
-    request(
-      'http://localhost:8080/api/products/',
-      function (error, response, body) {
-        const { products } = JSON.parse(body);
-        expect(products.length).to.equal(44);
-        done();
-      }
-    );
-  });
+	it('Check total numbers of items', function (done) {
+		request(
+			'https://skatebordslol.herokuapp.com/api/products/',
+			function (error, response, body) {
+				const { products } = JSON.parse(body);
+				expect(products.length).to.equal(44);
+				done();
+			}
+		);
+	});
 
-  it('Check type of product (truck)', function (done) {
-    request(
-      'http://localhost:8080/api/products/?category=truck',
-      function (error, response, body) {
-        const { products } = JSON.parse(body);
-        const isTrucks = products.every((product) => product.type === 'truck');
-        expect(isTrucks).to.equal(true);
-        done();
-      }
-    );
-  });
-  it('Check type of product (skateboard)', function (done) {
-    request(
-      'http://localhost:8080/api/products/?category=skateboard',
-      function (error, response, body) {
-        const { products } = JSON.parse(body);
-        const isTrucks = products.every(
-          (product) => product.type === 'skateboard'
-        );
-        expect(isTrucks).to.equal(true);
-        done();
-      }
-    );
-  });
-  it('Check type of product (wheels)', function (done) {
-    request(
-      'http://localhost:8080/api/products/?category=wheels',
-      function (error, response, body) {
-        const { products } = JSON.parse(body);
-        const isTrucks = products.every((product) => product.type === 'wheels');
-        expect(isTrucks).to.equal(true);
-        done();
-      }
-    );
-  });
+	it('Check type of product (truck)', function (done) {
+		request(
+			'https://skatebordslol.herokuapp.com/api/products/?category=truck',
+			function (error, response, body) {
+				const { products } = JSON.parse(body);
+				const isTrucks = products.every((product) => product.type === 'truck');
+				expect(isTrucks).to.equal(true);
+				done();
+			}
+		);
+	});
+	it('Check type of product (skateboard)', function (done) {
+		request(
+			'https://skatebordslol.herokuapp.com/api/products/?category=skateboard',
+			function (error, response, body) {
+				const { products } = JSON.parse(body);
+				const isTrucks = products.every(
+					(product) => product.type === 'skateboard'
+				);
+				expect(isTrucks).to.equal(true);
+				done();
+			}
+		);
+	});
+	it('Check type of product (wheels)', function (done) {
+		request(
+			'https://skatebordslol.herokuapp.com/api/products/?category=wheels',
+			function (error, response, body) {
+				const { products } = JSON.parse(body);
+				const isTrucks = products.every((product) => product.type === 'wheels');
+				expect(isTrucks).to.equal(true);
+				done();
+			}
+		);
+	});
 });
